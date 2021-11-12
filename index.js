4d3ceb0398e5cb7598b0f8392732087a36ba83e3
ArifRexa <arif.reza3126@gmail.com>
2021-11-12 16:46:15 +0600

    watches delete

--- index.js
+++ index.js
@@ -38,6 +38,13 @@ async function run() {
       res.json(result)
     })
 
+    app.delete("/watches/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await watchesCollection.deleteOne(query);
+      res.json(result)
+    })
+
 
 
     app.get("/orders", async (req, res) => {
