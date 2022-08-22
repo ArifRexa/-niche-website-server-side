f8b6cc86485c3cac972d06b658ab47f6469d16e6
Arif reza <arif.reza3126@gmail.com>
2022-08-22 21:53:54 +0600

    mongo recovered

--- index.js
+++ index.js
@@ -11,9 +11,7 @@ app.use(cors())
 app.use(express.json());
 
 
-
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.h6t6k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.h6t6k.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
